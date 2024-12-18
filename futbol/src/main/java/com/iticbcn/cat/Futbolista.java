a869fb921c57b7d90e8ecfc58006ab89debf70ab
Maria Amzil <mini.mar@hotmail.com>
2024-12-18 20:25:53 +0100

    Activitat 02-Futbol: commit final de classe

--- futbol/src/main/java/com/iticbcn/cat/Futbolista.java
+++ futbol/src/main/java/com/iticbcn/cat/Futbolista.java
@@ -34,4 +34,41 @@ public class Futbolista extends Thread {
             }
         }
     }
+
+    public static void main(String[] args) {
+        Futbolista[] jugadors = new Futbolista[Futbolista.NUM_JUGADORS];
+
+        String[] noms = { "Piqué", "Vinicius", "Torres", "Ramos", "Ronaldo",
+                          "Lewan", "Belli", "Arnau", "Aspas", "Messi", "MBapé" };
+
+        /*Popular llista de Futbolistes amb els noms*/ 
+        for (int i = 0; i < Futbolista.NUM_JUGADORS; i++) {
+            jugadors[i] = new Futbolista(noms[i]);
+        }
+
+        System.out.println("Inici dels xuts --------------------");
+
+        /*Iniciar fil per cada jugadaor */
+        for (Futbolista jugador : jugadors) {
+            jugador.start();
+        }
+
+        /* Esperar a que el fil de cada jugador acabi */
+        for (Futbolista jugador : jugadors) {
+            try {
+                jugador.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("Fi dels xuts -----------------------");
+
+        /*Mostrar les estadístiques */
+        System.out.println("--- Estadístiques ------");
+        for (Futbolista jugador : jugadors) {
+            /*Utilitzar el mètode getName de la classe Thread de la que hereda Futbolista */
+            System.out.println(jugador.getName() + " -> " + jugador.getNgols() + " gols");
+        }
+    }
 }
